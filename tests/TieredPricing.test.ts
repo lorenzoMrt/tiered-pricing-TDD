1362ec3c7ffe6d23a6b052ddbcd49ab2fbd45bab
lorenzo miret <lmiret@biobam.com>
2023-05-22 10:34:09 +0200

    Tiered pricing 1st approach

---
+++ tests/TieredPricing.test.ts
@@ -0,0 +1,13 @@
+import TieredPricing from "../src/TieredPricing";
+
+describe("TODO", () => {
+	test("TODO: rename later", () => {
+		const pricing: TieredPricing = new TieredPricing(2);
+		expect(pricing.total()).toBe(598);
+	});
+
+	test("TODO: rename later", () => {
+		const pricing: TieredPricing = new TieredPricing(3);
+		expect(pricing.total()).toBe(717);
+	});
+});
